73308e125701c50fff02e9ecb0199db4728ca67d
VickyMoony <42359040+VickyMoony@users.noreply.github.com>
2019-09-25 08:18:43 -0300

    mudanças

--- passar/php/contato.php
+++ passar/php/contato.php
@@ -4,20 +4,27 @@ $url="localhost";
 $usuario="root";
 $senha="";
 $bd = "passarbem";
-$nome = $_POST['nome']
-$email = $_POST['email']
-$tel = $_POST['tel']
-$conteudo = $_POST['conteudo']
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$tel = $_POST['tel'];
+$conteudo = $_POST['conteudo'];
 
+$nome = utf8_decode($nome);
+$email = utf8_decode($email);
+$tel = utf8_decode($tel);
+$conteudo = utf8_decode($conteudo);
 
 $con = mysqli_connect($url, $usuario, $senha, $bd);
 if (!$con){
 	die("Falha de conexão: ".mysqli_connect_error());
-} else{
+} 
 	echo "Conectado!";
-}
 
-$sql="INSERT INTO contato VALUES ('$nome', '$email', '$tel', $conteudo)";
-mysqli_query($con,$sql)
+$sql="INSERT INTO contato (nome, email, tel, conteudo) VALUES ('$nome', '$email', '$tel', '$conteudo')";
+if (mysqli_query($con,$sql)){
+	echo "<br> Inserido com sucesso";}
+	else{
+		echo "Erro: ".$sql."<br>".mysqli_error($con);
+}
 
 ?>
